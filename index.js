05eb0379ded717445a71192dec4aaec22a1ac36f
manishsaraan <manish021js@gmail.com>
2017-11-07 22:19:09 +0530

    adding app components

--- index.js
+++ index.js
@@ -3,12 +3,50 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles.css';
 import axios from 'axios';
+class Start extends React.Component{
+   render(){
+      return(
+            <div className="col-5">
+              <i className="fa fa-star"></i>
+              <i className="fa fa-star"></i>
+              <i className="fa fa-star"></i>
+              <i className="fa fa-star"></i>
+            </div>
+        );
+   }
+}
+
+class Button extends React.Component{
+   render(){
+      return(
+            <div className="col-2">
+              <button>=</button>
+            </div>
+        );
+   }
+}
+
+class Answer extends React.Component{
+   render(){
+      return(
+            <div className="col-5">
+              ....
+            </div>
+        );
+   }
+}
 
 class Game extends React.Component{
     render(){
        return(
-              <div>
-                <h3>App</h3>
+              <div className="container">
+                <h3>Play Nine</h3>
+                <hr/>
+                <div  className="row">
+                  <Start/>
+                  <Button/>
+                  <Answer/>
+                </div>
               </div>
         );
     }
