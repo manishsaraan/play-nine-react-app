7c112c8576ee3c44c7a4312d5e770cd3e4bd8715
manishsaraan <manish021js@gmail.com>
2017-11-09 22:57:00 +0530

    selecting, unselecting number and ui improvement

--- index.js
+++ index.js
@@ -4,49 +4,50 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles.css';
 import axios from 'axios';
 const Stars = (props) => {
-  const  numberOfStars = 1 + Math.floor(Math.random() * 9);
+ // const  numberOfStars = 1 + Math.floor(Math.random() * 9);
   // let stars = [];
   // for(let i=0;i<numberOfStars;i++){
   //    stars.push(<i key={i} className="fa fa-star"></i>);
   // }
        return(
             <div className="col-5">
-             {_.range(numberOfStars).map(i => 
+             {_.range(props.numberOfStars).map(i => 
                   <i key={i} className="fa fa-star"></i>
              )}
             </div>
         );
 }
 
-class Button extends React.Component{
-   render(){
+const Button  = (props) => {
       return(
             <div className="col-2">
-              <button>=</button>
+              <button  disabled={ props.selectedNumbers.length === 0 } className="btn">=</button>
             </div>
-        );
-   }
+        );  
 }
 
-class Answer extends React.Component{
-   render(){
+const Answer =  (props) => {
       return(
             <div className="col-5">
-              <span>5</span>
-              <span>6</span>
+             {props.selectedNumbers.map((number,i) => 
+                  <span key={i} onClick={() => props.unselectNumber(number)}>{number}</span>
+             )}
             </div>
         );
-   }
 }
 
 const Numbers = (props) => {
   // const arrayOfNumbers = _.range(1, 10);
-   
+   const numberClassName = (number) => {
+     return ( props.selectedNumbers.indexOf(number) >= 0 )? "selected" : "";
+   }
    return(
         <div className="card text-center">
            <div>
               {Numbers.list.map((number, i) => 
-                  <span key={i}>{number}</span>
+                  <span key={i} className={numberClassName(number)}
+                  onClick={() => props.selectNumber(number)}
+                  >{number}</span>
               )}
            </div>
         </div>
@@ -57,23 +58,35 @@ Numbers.list  =  _.range(1, 10);
 
 class App extends React.Component{
 state = {
-   selectedNumbers : [2,4]
+   selectedNumbers : [],
+   randomNumberOfStars: 1 + Math.floor(Math.random() * 9)
+}
+selectNumber = (clicked) => {
+    if(this.state.selectedNumbers.indexOf(clicked) !== -1) {return;}
+    this.setState({selectedNumbers : this.state.selectedNumbers.concat(clicked)});
+};
+unselectNumber = (clicked) => {
+    this.setState({selectedNumbers : this.state.selectedNumbers.
+                                     filter(number => number !== clicked)});
 }
    render(){
+     const { selectedNumbers, randomNumberOfStars } = this.state;
      return(
              <div className="container">
              <h3>Play Nine</h3>
              <hr/>
              <div className="row">
-                <Stars/>
-                <Button/>
-                <Answer selectedNumbers = {this.state.selectedNumbers}/>
+                <Stars numberOfStars = {randomNumberOfStars}/>
+                <Button selectedNumbers = {selectedNumbers}/>
+                <Answer selectedNumbers = {selectedNumbers}
+                        unselectNumber = {this.unselectNumber}/>
              </div>
               <br/>
-               <Numbers/>
+               <Numbers selectedNumbers = {selectedNumbers}
+                         selectNumber = {this.selectNumber}/>
              </div>
        );
    }
 }
-
+ 
 ReactDOM.render(<App/>,document.getElementById('container'))
